9dd34a7febb0e59ae6cd2bc60ee1582f1f1a7a57
Jjosebalanta <jbalanta913@gmail.com>
2024-10-21 20:23:40 +0000

    fin

---
+++ Ejeciciodia21.java
@@ -0,0 +1,4 @@
+public class Ejeciciodia21 {
+    
+    
+}
--- Ejercicio.java
+++ Ejercicio.java
@@ -1,48 +1,92 @@
- import java . util . Random ;
- import java . util . Scanner ;
+import java.util.Random;
+import java.util.Scanner;
 
- class Personaje {
- private String nombre ;
- private int puntosDeVida ;
- private final int MAX_DANO = 30;
- private final int MIN_DANO = 10;
+class Personaje {
+    private String nombre;
+    private int puntosDeVida;
+    private final int MAX_DANO = 30;
+    private final int MIN_DANO = 10;
 
- // Constructor para inicializar los datos del personaje
- public Personaje ( String nombre ) {
- this . nombre = nombre ;
- this . puntosDeVida = 100; // Todos comienzan con 100puntos de vida
- }
+    public Personaje(String nombre) {
+        this.nombre = nombre;
+        this.puntosDeVida = 100;
+    }
 
- // Metodo para realizar un ataque a otro personaje
- public void atacar ( Personaje oponente ) {
- Random rand = new Random () ;
- int dano = rand . nextInt (( MAX_DANO - MIN_DANO ) + 1) +
-MIN_DANO ; // Dano entre 10 y 30
- oponente . recibirDano ( dano ) ;
- System . out . println ( this . nombre + " ataca a " +
-oponente . getNombre () + " causando " + dano + "puntos de dano .") ;
- }
+    public void atacar(Personaje oponente) {
+        Random rand = new Random();
+        int dano = rand.nextInt((MAX_DANO - MIN_DANO) + 1) +
+                MIN_DANO;
+        oponente.recibirDano(dano);
+        System.out.print(this.nombre + " ataca a " +
+                oponente.getNombre() + " causando " + dano + "puntos de dano .");
+    }
 
- // Metodo para recibir dano
- public void recibirDano ( int dano ) {
- this . puntosDeVida -= dano ;
- if ( this . puntosDeVida < 0) {
- this . puntosDeVida = 0; // No se puede tenermenos de 0 puntos de vida
- }
- }
+    public void recibirDano(int dano) {
+        this.puntosDeVida -= dano;
+        if (this.puntosDeVida < 0) {
+            this.puntosDeVida = 0;
+        }
+    }
 
- // Verifica si el personaje sigue vivo
- public boolean estaVivo () {
- return this . puntosDeVida > 0;
- }
+    public boolean estaVivo() {
+        return this.puntosDeVida > 0;
+    }
 
- // Devuelve el nombre del personaje
- public String getNombre () {
- return this . nombre ;
- }
+    public String getNombre() {
+        return this.nombre;
+    }
 
- // Devuelve los puntos de vida actuales
- public int getPuntosDeVida () {
- return this . puntosDeVida ;
- }
- }
+    public int getPuntosDeVida() {
+        return this.puntosDeVida;
+    }
+}
+
+class JuegoLucha {
+    private Personaje jugador1;
+    private Personaje jugador2;
+
+    public JuegoLucha(String nombre1, String nombre2) {
+
+        this.jugador1 = new Personaje(nombre1);
+        this.jugador2 = new Personaje(nombre2);
+    }
+
+    public void iniciarPelea() {
+        System.out.print("La pelea comienza entre " +
+                jugador1.getNombre() + " y " + jugador2.getNombre() + "...");
+
+        while (jugador1.estaVivo() && jugador2.estaVivo()) {
+            turno(jugador1, jugador2);
+            if (jugador2.estaVivo()) {
+                turno(jugador2, jugador1);
+            }
+        }
+
+        if (jugador1.estaVivo()) {
+            System.out.print(jugador1.getNombre() + " haganado la pelea .");
+        } else {
+            System.out.print(jugador2.getNombre() + " haganado la pelea .");
+        }
+    }
+
+    private void turno(Personaje atacante, Personaje defensor) {
+        System.out.print(" Turno de " + atacante.getNombre() + ". Puntos de vida de " + defensor.getNombre() + ": "
+                + defensor.getPuntosDeVida());
+        atacante.atacar(defensor);
+        System.out.print(defensor.getNombre() + " ahoratiene " + defensor.getPuntosDeVida() + " puntosde vida .");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" Introduce el nombre del jugador 1:");
+        String nombre1 = scanner.nextLine();
+
+        System.out.print(" Introduce el nombre del jugador 2:");
+        String nombre2 = scanner.nextLine();
+
+        JuegoLucha juego = new JuegoLucha(nombre1, nombre2);
+        juego.iniciarPelea();
+    }
+    
+}
