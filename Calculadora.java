e78538f98262c018f8591739cab07a19b256580e
Jjosebalanta <jbalanta913@gmail.com>
2024-10-25 23:39:20 +0000

    fin

--- Calculadora.java
+++ Calculadora.java
@@ -1,13 +1,30 @@
 public class Calculadora {
    
-         private int suma;
-         private int resta ;
-         private  double multiplicacin ;
-         private double divicion; 
+         private int valor1;
+         private int valor2 ;
         
-        public Calculadora(int suma,int resta,double multiplicacin,double divicion  ){
+        
+         public Calculadora(int valor1,int valor2){
+            this.valor1=valor1;
+            this.valor2=valor2;
             
-        }
+         }
+         public  int suma(){
+            int suma= this.valor1+this.valor2;
+            return suma;
+         }
+         public int resta(){
+                int resta= this.valor1-this.valor2;
+                return resta;
+         }
+         public int multiplicacion(){
+                int multiplicacion=this.valor1*this.valor2;
+                return multiplicacion;
+         }
+         public double division(){
+                double division=this.valor1/this.valor2;
+                return division;
+         }
 
 
 
--- Calculo.java
+++ Calculo.java
@@ -1,6 +1,18 @@
 public class Calculo {
  public static void main(String[] args) {
-    Calculadora objp1 = new Calculadora ();
+    Calculadora objp1 = new Calculadora (10,3);
+
+    int suma = objp1.suma();
+    System.out.println("la suma es: "+suma);
+
+    int resta = objp1.resta();
+    System.out.println("la resta es: "+resta);
+
+    int multiplicacion = objp1.multiplicacion();
+    System.out.println("la multiplicacion es: "+multiplicacion);
+
+    double division = objp1.division();
+    System.out.println("la division es: "+division);
     
  }
     
