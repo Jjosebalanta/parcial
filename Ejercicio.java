a4dffc442ffc0c4071cd1c343afa0213c84da7c9
Jjosebalanta <jbalanta913@gmail.com>
2024-10-28 18:47:53 +0000

    fin

--- Ejercicio.java
+++ Ejercicio.java
@@ -2,6 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Personaje {
+   private String cuchillo;
     private String nombre;
     private int puntosDeVida;
     //en mayusculas porque es constate y el "private final" es porque no va a cambiar***
@@ -16,17 +17,26 @@ class Personaje {
     public void atacar(Personaje oponente) {
         Random rand = new Random();
         //para de clarar un aleatorio rand
+        int incremeto=20;
         int dano = rand.nextInt((MAX_DANO - MIN_DANO) + 1) +
                 MIN_DANO;
         oponente.recibirDano(dano);
         System.out.print(this.nombre + " ataca a " +
                 oponente.getNombre() + " causando " + dano + "puntos de dano .");
+        oponente.recibirincrimetodevida(incremeto);  
+            
     }
 
     public void recibirDano(int dano) {
         this.puntosDeVida -= dano;
         if (this.puntosDeVida < 0) {
             this.puntosDeVida = 0;
+        }
+    }
+    public void recibirincrimetodevida(int incremeto) {
+        this.puntosDeVida +=incremeto ;
+        if (this.puntosDeVida < 15) {
+            this.puntosDeVida +=20;
         }
     }
 
@@ -41,6 +51,9 @@ class Personaje {
     public int getPuntosDeVida() {
         return this.puntosDeVida;
     }
+    public String cuchillo(){
+        return this.cuchillo;
+    }
 }
 
 class JuegoLucha {
@@ -52,25 +65,33 @@ class JuegoLucha {
         this.jugador1 = new Personaje(nombre1);
         this.jugador2 = new Personaje(nombre2);
     }
-
+    
     public void iniciarPelea() {
         System.out.print("La pelea comienza entre " +
-                jugador1.getNombre() + " y " + jugador2.getNombre() + "...");
+                jugador1.getNombre() + " y " + jugador2.getNombre() + " ");
+                
+                
+        System.out.print("se asigna el arma cuchillo a "+
+        jugador1.getNombre()+".");
+        System.out.print("se asigna el arma manopla a "+
+        jugador2.getNombre()+".");
+
 
         while (jugador1.estaVivo() && jugador2.estaVivo()) {
             turno(jugador1, jugador2);
             if (jugador2.estaVivo()) {
                 turno(jugador2, jugador1);
+
             }
         }
-
+    
         if (jugador1.estaVivo()) {
             System.out.print(jugador1.getNombre() + " haganado la pelea .");
         } else {
             System.out.print(jugador2.getNombre() + " haganado la pelea .");
         }
     }
-
+    
     private void turno(Personaje atacante, Personaje defensor) {
         System.out.print(" Turno de " + atacante.getNombre() + ". Puntos de vida de " + defensor.getNombre() + ": "
                 + defensor.getPuntosDeVida());
